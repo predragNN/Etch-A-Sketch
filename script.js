0ac9f5213a210197cb52bbb8bddf5bcd0886c810
predragNN <nik.predrag998@gmail.com>
2024-06-05 01:51:59 +0200

    Coloring, reseting function. Remaining: change size, rainbow mode

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const colorBtn = document.querySelector("#colorButton");
+const eraseBtn = document.querySelector("#erase-button");
 const colorPick = document.querySelector("#color-picker");
-let color ="";
+const grid = document.querySelector(".main");
+
+let color ="#808080";
+let pixels = 64;
+
+
+let divsInRow = 8;
+for (let i=0; i<pixels; i++){
+    const div = document.createElement("div");
+    div.classList.add("kid");
+    div.style.cssText=`flex: 1 1 calc(100% / ${divsInRow}); border: 1px solid gray;`;
+    grid.appendChild(div);
+}
 
 colorBtn.addEventListener("click", ()=>{
     colorPick.click();
@@ -11,3 +24,28 @@ colorPick.addEventListener("input", ()=>{
     console.log(color);
     colorBtn.style.backgroundColor=color;
 });
+
+
+const kids = document.querySelectorAll(".kid");
+
+let mouseDown = false;
+document.addEventListener("mousedown",()=>{
+    mouseDown = true;
+});
+document.addEventListener("mouseup",()=>{
+    mouseDown = false;
+});
+
+kids.forEach(kid => {
+    kid.addEventListener("mouseover", ()=>{
+        if(mouseDown){
+            kid.style.backgroundColor = color;
+        }
+    });
+});
+
+eraseBtn.addEventListener("click",()=>{
+    kids.forEach(kid => {
+        kid.style.backgroundColor = "white";
+    });
+})
